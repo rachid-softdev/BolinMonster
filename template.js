6936d00e46b4f580c5fbc8df0f1cade87ec92454
Odgen <46386572+BolinMonster@users.noreply.github.com>
2026-08-08 07:27:20 +0000

    Initial Commit

---
+++ template.js
@@ -0,0 +1,56 @@
+module.exports = `
+
+### Hi there, I'm Rachid👋
+
+[![MasterHead](https://zupimages.net/up/23/33/irwb.gif)](https://github.com/BolinMonster)
+
+<!--
+**BolinMonster/BolinMonster** is a ✨ _special_ ✨ repository because its \`README.md\` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
+
+#### Languages
+
+<img align="left" alt="Java" src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"/>
+<img align="left" alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"/>
+<img align="left" alt="PHP" src="https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white"/>
+<img alt="C" src="https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white"/>
+
+#### Frontend
+
+<img align="left" alt="Bootstrap" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white"/>
+<img alt="Tailwind CSS" src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white"/>
+
+#### Backend
+
+<img align="left" alt="Node.js" src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"/>
+<img alt="Spring" src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white"/>
+
+#### Database
+
+<img align="left" alt="PostgreSQL" src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white"/>
+
+#### Statistics
+
+<img align="left" width="47%" src="https://github-readme-stats.vercel.app/api?username=BolinMonster&show_icons=true&theme=algolia" />
+<img width="47%" src="https://github-readme-stats.vercel.app/api/top-langs/?username=BolinMonster&layout=compact" />
+
+### Visitor count
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=BolinMonster&label=Profile%20views&color=0e75b6&style=flat-square" alt="BolinMonster" /> </p>
+
+Last update on {today_date}
+
+🤖 This README.md is updated daily, by Actions ❤️
+
+`;
